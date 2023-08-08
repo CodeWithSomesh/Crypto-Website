dc0d013f376c313cb0b56d1498da7da3ea33ab57
CodeWithSomesh <codewithsomesh@gmail.com>
2023-08-09 01:59:46 +0800

    Making it suitable for deployment

--- src/routers/CoinDetails.jsx
+++ src/routers/CoinDetails.jsx
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import axios from 'axios'
-import DOMPurify, { sanitize } from 'dompurify'
+import DOMPurify from 'dompurify'
 
 const CoinDetails = () => {
 
